fd9271375d456071203cbb9f1fb258aba2236076
1iph <nmcclung9@gmail.com>
2025-05-19 20:18:06 +0000

    Test commit

--- script.js
+++ script.js
@@ -17,21 +17,21 @@ const progressBar = document.getElementById("progress");
 // Quiz questions
 const quizQuestions = [
   {
-    question: "What is the capital of France?",
+    question: "What term is given to the scientific study of all aspects of the life of fungi?",
     answers: [
-      { text: "London", correct: false },
-      { text: "Berlin", correct: false },
-      { text: "Paris", correct: true },
-      { text: "Madrid", correct: false },
+      { text: "Fungology", correct: false },
+      { text: "Fungism", correct: false },
+      { text: "Etymology", correct: false },
+      { text: "Mycology", correct: true },
     ],
   },
   {
-    question: "Which planet is known as the Red Planet?",
+    question: "What was the profession of Einstein's first wife, Mileva Maric?",
     answers: [
-      { text: "Venus", correct: false },
-      { text: "Mars", correct: true },
-      { text: "Jupiter", correct: false },
-      { text: "Saturn", correct: false },
+      { text: "Psycologist", correct: false },
+      { text: "Physicist", correct: true },
+      { text: "Chiropractor", correct: false },
+      { text: "Biologist", correct: false },
     ],
   },
   {
@@ -46,19 +46,82 @@ const quizQuestions = [
   {
     question: "Which of these is NOT a programming language?",
     answers: [
-      { text: "Java", correct: false },
+      { text: "Swift", correct: false },
       { text: "Python", correct: false },
-      { text: "Banana", correct: true },
+      { text: "Apple", correct: true },
       { text: "JavaScript", correct: false },
     ],
   },
   {
-    question: "What is the chemical symbol for gold?",
+    question: "What is the chemical symbol for iron?",
     answers: [
-      { text: "Go", correct: false },
-      { text: "Gd", correct: false },
-      { text: "Au", correct: true },
-      { text: "Ag", correct: false },
+      { text: "Ir", correct: false },
+      { text: "Fa", correct: false },
+      { text: "Fe", correct: true },
+      { text: "In", correct: false },
+    ],
+  },
+  {
+    question: "What is the hardest natural substance on Earth?",
+    answers: [
+      { text: "Obsidian", correct: false },
+      { text: "Cast Iron", correct: false },
+      { text: "Steel", correct: false },
+      { text: "Diamond", correct: true },
+    ],
+  },
+   {
+    question: "Who wrote The Odyssey?",
+    answers: [
+      { text: "William Shakespear", correct: false },
+      { text: "Charles Dickens", correct: false },
+      { text: "Dr. Seuss", correct: false },
+      { text: "Homer", correct: true },
+    ],
+  },
+   {
+    question: "What is the term for a word that is similar in meaning to another word?",
+    answers: [
+      { text: "Cinnamon", correct: false },
+      { text: "Synonym", correct: true },
+      { text: "Ananyms", correct: false },
+      { text: "Homonyms", correct: false },
+    ],
+  },
+   {
+    question: "What year was the first iPhone released?",
+    answers: [
+      { text: "2004", correct: false },
+      { text: "2000", correct: false },
+      { text: "2007", correct: true },
+      { text: "2008", correct: false },
+    ],
+  },
+  {
+    question: "What is the most spoken language in the world?",
+    answers: [
+      { text: "English", correct: false },
+      { text: "Spanish", correct: false },
+      { text: "Mandarin Chinese", correct: true },
+      { text: "Hindi", correct: false },
+    ],
+  },
+  {
+    question: "What is the most spoken language in the world?",
+    answers: [
+      { text: "English", correct: false },
+      { text: "Spanish", correct: false },
+      { text: "Mandarin Chinese", correct: true },
+      { text: "Hindi", correct: false },
+    ],
+  },
+  {
+    question: "What is the smallest bone in the human body?",
+    answers: [
+      { text: "incus", correct: false },
+      { text: "Xiphoid process", correct: false },
+      { text: "Stapes", correct: true },
+      { text: "Coccyx", correct: false },
     ],
   },
 ];
